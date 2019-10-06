67900de47c4b1c5e43f2539fd66d76d700d4da18
Oskars Putans <o.putaans@inbox.lv>
2019-10-06 16:27:15 +0000

    dragging from the right screen to the left now actually possible

---
+++ Assets/Hb_Disabler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hb_Disabler : MonoBehaviour
+{
+    bool on = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (on)
+        {
+            if (transform.childCount == 0)
+            {
+                on = false;
+                foreach(var script in Activat.instances)
+                {
+                    script.SetHb(on);
+                }
+            }
+        }
+        else
+        {
+            if (transform.childCount == 1)
+            {
+                on = true;
+                foreach (var script in Activat.instances)
+                {
+                    script.SetHb(on);
+                }
+            }
+        }
+    }
+}
--- Assets/Script/Activat.cs
+++ Assets/Script/Activat.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Activat : MonoBehaviour
 {
     // Start is called before the first frame update
+    public static List<Activat> instances=new List<Activat>();
     void Start()
     {
-        
+        instances.Add(transform.GetComponent<Activat>());
     }
-
+    public void SetHb(bool active)
+    {
+        transform.GetComponent<BoxCollider2D>().enabled = active;
+    }
     // Update is called once per frame
     void Update()
     {
