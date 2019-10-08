5bf86099de952d51b76f2e63f840abeffcfe798b
Lorenss Martinsons <lorensytas@gmail.com>
2019-10-08 18:18:57 +0000

    button sounds

---
+++ Assets/Script/buttonSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class buttonSound : MonoBehaviour
+{
+    private Button yourButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        yourButton = transform.GetComponent<Button>();
+        yourButton.onClick.AddListener(Go);
+    }
+    void Go()
+    {
+        transform.GetComponent<AudioSource>().Play();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
