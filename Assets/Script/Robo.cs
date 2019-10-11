2e05a8c35ce98023a3f33e23e5578d6f5c3cb471
Lorenss Martinsons <lorensytas@gmail.com>
2019-10-11 13:35:13 +0000

    rob sounds update

--- Assets/Script/Robo.cs
+++ Assets/Script/Robo.cs
@@ -43,12 +43,12 @@ public class Robo : MonoBehaviour
             Receptionist.GetComponent<receptionist>().Test();
             receptionist.reputation_score += 20;
             if (receptionist.reputation_score > 100) receptionist.reputation_score = 100;
-            GameObject.Find("RobSounds").GetComponent<AudioSource>().clip = Resources.Load<AudioClip>("sounds/SFX_VITC_3");
+            GameObject.Find("RobSounds").GetComponent<AudioSource>().clip = Resources.Load<AudioClip>("sounds/SFX_VITC_8");
             GameObject.Find("RobSounds").GetComponent<AudioSource>().Play();
         }
         else
         {
-            GameObject.Find("RobSounds").GetComponent<AudioSource>().clip = Resources.Load<AudioClip>("sounds/SFX_VITC_2");
+            GameObject.Find("RobSounds").GetComponent<AudioSource>().clip = Resources.Load<AudioClip>("sounds/SFX_VITC_9");
             GameObject.Find("RobSounds").GetComponent<AudioSource>().Play();
             receptionist.reputation_score -= 10;
             if (receptionist.reputation_score < 20) receptionist.reputation_score = 20;
